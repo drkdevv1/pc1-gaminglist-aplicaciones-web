c95968f4388a68bb8e5c945239cdebfb53d1c8ae
Jose Calderon <103902831+drkdevv1@users.noreply.github.com>
2024-04-27 12:52:17 -0500

    Update footer-component.component.vue

--- src/public/components/footer-component.component.vue
+++ src/public/components/footer-component.component.vue
@@ -8,7 +8,7 @@ export default {
 <template>
   <div class="c-footer">
     <p>Copyright © 2024 Gaming List, inc. All rights reserved.</p>
-    <p>Developed by U202213076 - José Daniel Mario Calderón Huamán </p>
+    <p>Developed by José Calderón </p>
   </div>
 </template>
 
